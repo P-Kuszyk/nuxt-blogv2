b05f5107ac080eeba8cbf125465144023b5e72c6
P-Kuszyk <p.kuszyk1221@wp.pl>
2024-04-26 13:31:29 +0200

    Change logo

--- components/Top/Nav.vue
+++ components/Top/Nav.vue
@@ -10,7 +10,7 @@ const showMenu = () => {
  <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      <img src="https://bulma.io/assets/brand/Bulma%20Logo%20Black.png" width="112" height="28">
     </a>
 
     <a role="button" 
