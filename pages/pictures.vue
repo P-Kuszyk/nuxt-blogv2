27d8513fd96b361ef9122851b4d54e8389ab3b08
P-Kuszyk <p.kuszyk1221@wp.pl>
2024-04-29 15:31:50 +0200

    Add pictures
    Add pictures

--- pages/pictures.vue
+++ pages/pictures.vue
@@ -1,21 +1,39 @@
-<template>
-    <div class="SpacePicture">
-      <img class="card" :src="img" />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Picture',
-    props: {
-      id: {
-        type: Number,
-      },
-      img: {
-        type: String,
-        reqiured: true,
-      },
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Picture',
+  data() {
+    return {
+      img: '' 
+    };
+  },
+  mounted() {
+
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+
+      axios.get('http://localhost:8000/Picture')
+  .then(response => {
+
+    if (response.data && response.data.length > 0) {
+        this.img = response.data[0].img;
+    } else {
+      console.error('Odpowiedź serwera jest pusta.');
+    }
+  })
+  .catch(error => {
+    console.error('Błąd pobierania danych:', error);
+  });
     }
   }
-  </script>
-  
+}
+</script>
+
+<template>
+  <div class="SpacePicture">
+    <img class="card" :src="img" />
+  </div>
+</template>
