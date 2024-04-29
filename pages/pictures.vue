3647004ef2222717e429429edbbccd2a61f09df2
P-Kuszyk <p.kuszyk1221@wp.pl>
2024-04-29 13:20:09 +0200

    Update pictures.vue

--- pages/pictures.vue
+++ pages/pictures.vue
@@ -1,36 +1,20 @@
 <template>
-    <div class="dinner">
-      <h1>{{ title }}</h1>
+    <div class="SpacePicture">
       <img class="card" :src="img" />
-      <h3>Ocena klientów {{ opinion }}/5</h3>
-      <h5>{{ comment }}</h5>
     </div>
   </template>
   
   <script>
   export default {
-    name: 'DinnerComponent',
+    name: 'Picture',
     props: {
       id: {
         type: Number,
-        default: () => Math.floor(Math.random() * 1000), // Losowe id
-      },
-      title: {
-        type: String,
-        default: () => "Danie dnia", // Domyślny tytuł
       },
       img: {
         type: String,
-        default: () => "https://placekitten.com/200/300", // Domyślny obrazek (kotek)
+        reqiured: true,
       },
-      opinion: {
-        type: Number,
-        default: () => Math.floor(Math.random() * 5) + 1, // Losowa ocena od 1 do 5
-      },
-      comment: {
-        type: String,
-        default: () => "Brak komentarza", // Domyślny komentarz
-      }
     }
   }
   </script>
