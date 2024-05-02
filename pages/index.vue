2d1251dd9e0c5bbda23e1840e14624c9beb8eb56
P-Kuszyk <p.kuszyk1221@wp.pl>
2024-05-02 13:40:47 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
     h3{
         font-size: 200px;
         color: gold;
-        animation: neon 15s infinite alternate;
+        animation: neon 10s infinite alternate;
     }
 
     @keyframes neon{
@@ -29,94 +29,28 @@
         opacity: 1;
       }
       10% {
-        opacity: 0;
-      }
-      10.1% {
-        opacity: 1;
-      }
-      10.2% {
-        opacity: 0;
+        opacity: 0.5;
       }
       20% {
-        opacity: 0;
-      }
-      20.1% {
-        opacity: 1;
-      }
-      20.6% {
-        opacity: 0;
+        opacity: 0.8;
       }
       30% {
-        opacity: 0;
-      }
-      30.1% {
-        opacity: 1;
-      }
-      30.5% {
-        opacity: 1;
-      }
-      30.6% {
-        opacity: 0;
+        opacity: 0.2;
       }
       45% {
-        opacity: 0;
-      }
-      45.1% {
-        opacity: 1;
-      }
-      50% {
-        opacity: 1;
-      }
-      55% {
-        opacity: 1;
-      }
-      55.1% {
-        opacity: 0;
-      }
-      57% {
-        opacity: 0;
-      }
-      57.1% {
-        opacity: 1;
-      }
-      60% {
-        opacity: 1;
-      }
-      60.1% {
-        opacity: 0;
+        opacity: 0.4;
       }
       65% {
-        opacity: 0;
+        opacity: 0.9;
       }
       65.1% {
-        opacity: 1;
+        opacity: 0.3;
       }
       75% {
         opacity: 1;
       }
-      75.1% {
-        opacity: 0;
-      }
-      77% {
-        opacity: 0;
-      }
-      77.1% {
-        opacity: 1;
-      }
-      85% {
-        opacity: 1;
-      }
-      85.1% {
-        opacity: 0;
-      }
-      86% {
-        opacity: 0;
-      }
-      86.1% {
-        opacity: 1;
-      }
       100% {
-        opacity: 1;
+        opacity: 0.6;
       }
     }
   }
